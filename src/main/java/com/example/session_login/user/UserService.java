39ec5cfff01d8676c79f0d89758487f851313b34
Hyzee2 <153643940+Hyzee2@users.noreply.github.com>
2024-12-21 12:20:37 +0900

    [Test] test4

--- src/main/java/com/example/session_login/user/UserService.java
+++ src/main/java/com/example/session_login/user/UserService.java
@@ -18,6 +18,6 @@ public class UserService {
     public String create(@RequestBody UserDto userDto) {
         User user = userDto.toEntity(passwordEncoder.encode(userDto.getPassword()));
         userRepository.save(user);
-        return "User registered successfully!!^_^";
+        return "User registered successfully!!^__^";
     }
 }
